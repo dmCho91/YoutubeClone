d2bcf091cdc2282d9130d3f079a6bb8535fdf9a7
YeoungHoon Cho <dm.cho91@gmail.com>
2020-08-11 16:48:53 +0900

    create Comment Model and Component

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from 'antd'
 import Axios from 'axios'
 import SideVideo from './Sections/SideVideo'
 import Subscribe from './Sections/Subscribe'
+import Comment from './Sections/Comment'
 
 function VideoDetailPage(props) {
 
@@ -49,6 +50,8 @@ function VideoDetailPage(props) {
                         </List.Item>
 
                         {/* comments */}
+                        <Comment />
+
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
